f297e46c398de09384226c1f581cec7ef11a4a8d
Richard A. Mead <579898+Hedrik@users.noreply.github.com>
2026-06-16 16:54:19 -0700

    Set font in .ini file (16 -> 17); update UIScale to have static methods, refactor sightly

--- src/xaos/utils/UIScale.java
+++ src/xaos/utils/UIScale.java
@@ -1,26 +1,37 @@
 package xaos.utils;
 
 public final class UIScale {
-    private static float scale = 1.0f;
+
+    private static final float MIN_FONT_SCALE = 1.0f;
+    private static final float MAX_FONT_SCALE = 3.0f;
+    private static final float DEFAULT_FONT_SCALE = 1.0f;
+
+    private static float scale = DEFAULT_FONT_SCALE;
+
+    private static transient volatile float fClampedVal = 0f;
 
     private UIScale() {}
 
-    public static float get() {
+    public static synchronized float get() {
         return scale;
     }
 
-    public static void set(float newScale) {
-        if (newScale < 1.0f) {
-            scale = 1.0f;
-        } else if (newScale > 3.0f) {
-            scale = 3.0f;
-        } else {
-            scale = newScale;
-        }
+    private static final synchronized float clamp(float fVal) {
+        fClampedVal = (!Float.isFinite(fVal)) ? DEFAULT_FONT_SCALE : fVal;
+        fClampedVal = Math.min(Math.max(fClampedVal,MIN_FONT_SCALE),MAX_FONT_SCALE);
+        return fClampedVal;
     }
 
-    public static int px(int value) {
-        return Math.round(value * scale);
+    public static synchronized void set(final float newScale) {
+        scale = clamp(newScale);
+    }
+
+    public static short px(final int value) {
+        return (short) (((float) value) * scale);
+    }
+
+    public static int textWidth(final String text) {
+        return (int) (((float) (UtilFont.getWidth(text))) * scale);
     }
 
     public static int fontWidth() {
@@ -31,7 +42,15 @@ public final class UIScale {
         return px(UtilFont.MAX_HEIGHT);
     }
 
-    public static int textWidth(String text) {
-        return px(UtilFont.getWidth(text));
+     public static synchronized int fontHeight(final float inScaleOverride) {
+        return ((inScaleOverride == 0f)?fontHeight():(int)(((float) UtilFont.MAX_HEIGHT) * clamp(inScaleOverride)));
     }
+
+/*
+    public static CharDef getCharDefScaled (int charID) {
+		CharDef cd = UtilFont.getCharDef (int charID);
+
+        return cd;
+    }
+*/
 }
